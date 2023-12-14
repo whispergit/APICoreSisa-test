438ffd2726faa1ad683920d7534f1330e6b608b3
STACiA <69411527+staciax@users.noreply.github.com>
2023-12-14 15:00:00 +0700

    `"` to `'`

--- tests/constants.py
+++ tests/constants.py
@@ -1,10 +1,10 @@
-# BASE: str = "https://localhost:7036"
-BASE: str = "http://172.20.24.63:80/new"
+# BASE: str = 'https://localhost:7036'
+BASE: str = 'http://172.20.24.63:80/new'
 
-ACCESS_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJBbmRyb2lkIiwiZnVsbE5hbWUiOiJBbmRyb2lkIEFQSSBDbGllbnQiLCJyb2xlIjoiQWRtaW4iLCJqdGkiOiIxMmIxOTlkYy03NWFhLTQzYmMtYWE2Zi01MDlkZDBiNjcyNmEiLCJleHAiOjE3MTAxMjMzNDgsImlzcyI6Imh0dHBzOi8vc3RvdS5hYy50aCIsImF1ZCI6Imh0dHBzOi8vc3RvdS5hYy50aCJ9._X72NLRtEyiasUkPvZoy9HxBgk32PS3GkqugoUcVqJ8"
+ACCESS_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJBbmRyb2lkIiwiZnVsbE5hbWUiOiJBbmRyb2lkIEFQSSBDbGllbnQiLCJyb2xlIjoiQWRtaW4iLCJqdGkiOiIxMmIxOTlkYy03NWFhLTQzYmMtYWE2Zi01MDlkZDBiNjcyNmEiLCJleHAiOjE3MTAxMjMzNDgsImlzcyI6Imh0dHBzOi8vc3RvdS5hYy50aCIsImF1ZCI6Imh0dHBzOi8vc3RvdS5hYy50aCJ9._X72NLRtEyiasUkPvZoy9HxBgk32PS3GkqugoUcVqJ8'
 
 HEADERS = {
-    "Authorization": "Bearer %s" % ACCESS_TOKEN,
+    'Authorization': 'Bearer %s' % ACCESS_TOKEN,
 }
 
 SSL_VERIFY = False
